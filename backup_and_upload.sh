96727f35e631edf2523252be2923c068f0905592
Diogo Cabral <diogo.cabral@gmail.com>
2024-08-02 16:36:51 -0300

    pgclient16

--- backup_and_upload.sh
+++ backup_and_upload.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -e
 
-FILENAME=${POSTGRES_DB}_`date +'%d_%m_%Y'`.backup
+FILENAME=${POSTGRES_DB}_`date +'%Y_%m_%d_%H%M%S'`.dump
 
 pg_dump -O -x --dbname=postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB} -v -F c -Z 9 -n public ${PG_DUMP_ARGS} -f ${FILENAME}
 
